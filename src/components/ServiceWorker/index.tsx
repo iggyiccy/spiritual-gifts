11b67b37872dd9f4d4d572033a4340f93ad14505
iggyi <iccyn@pm.me>
2024-09-03 23:38:53 +1000

    update

--- src/components/ServiceWorker/index.tsx
+++ src/components/ServiceWorker/index.tsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, useEffect } from "react";
 import useServiceWorker from "hooks/useServiceWorker";
 
 const InitialServiceWorkerInstallationDialog = lazy(
@@ -18,14 +18,21 @@ const ServiceWorker = () => {
   const { isInitialized, isUpdated, updateServiceWorker } = useServiceWorker();
   console.log("isInitialized:", isInitialized);
   console.log("isUpdated:", isUpdated);
-  return (
-    <>
-      {isInitialized && <InitialServiceWorkerInstallationDialog />}
-      {isUpdated && (
-        <UpdateForServiceWorkerAvailableDialog onUpdate={updateServiceWorker} />
-      )}
-    </>
-  );
+  useEffect(() => {
+    if (isUpdated) {
+      updateServiceWorker();
+    }
+  }, [isUpdated, updateServiceWorker]);
+
+  return null; // No need to render anything
+  // return (
+  //   <>
+  //     {isInitialized && <InitialServiceWorkerInstallationDialog />}
+  //     {isUpdated && (
+  //       <UpdateForServiceWorkerAvailableDialog onUpdate={updateServiceWorker} />
+  //     )}
+  //   </>
+  // );
 };
 
 export default ServiceWorker;
