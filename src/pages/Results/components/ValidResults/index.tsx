be91776571ebb08bb175014d925e8036bc5b3161
iggyi <iccyn@pm.me>
2024-09-06 10:58:16 +1000

    update

--- src/pages/Results/components/ValidResults/index.tsx
+++ src/pages/Results/components/ValidResults/index.tsx
@@ -10,6 +10,11 @@ import {
   TableRow,
 } from "../../../../components/ui/table";
 import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "../../../../components/ui/alert";
+import {
   PieChart,
   Pie,
   Cell,
@@ -17,6 +22,8 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import { useEffect, useState } from "react";
+import { Clock, Meh } from "lucide-react";
 
 localStorage.setItem("savedHref", window.location.href);
 
@@ -28,6 +35,49 @@ const COLORS = [
   "#9966FF",
   "#FF9F40",
 ];
+
+const CountdownClock = () => {
+  const calculateTimeLeft = () => {
+    const targetDate = new Date("2024-09-28T16:30:00");
+    const currentDate = new Date();
+    const difference = targetDate.getTime() - currentDate.getTime();
+
+    let timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [timeLeft]);
+
+  return (
+    <div className="pt-8 text-md text-primary font-bold">
+      {timeLeft.days} days {timeLeft.hours} hours {timeLeft.minutes} minutes{" "}
+      {timeLeft.seconds} seconds
+    </div>
+  );
+};
 
 const CustomPieChart = ({ results }: { results: Result[] }) => {
   const data = results.map((result) => ({
@@ -60,33 +110,66 @@ const CustomPieChart = ({ results }: { results: Result[] }) => {
   );
 };
 
-const ValidResults = ({ results }: { results: Result[] }) => (
-  <div className="flex flex-col space-y-4 text-center p-4 overflow-scroll text-primary">
-    <Helmet>
-      <title>屬靈恩賜評估結果</title>
-    </Helmet>
-    <h2 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
-      屬靈恩賜評估結果
-    </h2>
-    <div className="pb-8">
-      <CustomPieChart results={results} />
+const ValidResults = ({ results }: { results: Result[] }) => {
+  const [showResults, setShowResults] = useState(false);
+
+  useEffect(() => {
+    const targetDate = new Date("2024-09-28T16:30:00");
+    const currentDate = new Date();
+
+    if (currentDate >= targetDate) {
+      setShowResults(true);
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col space-y-4 text-center p-4 overflow-scroll text-primary">
+      <Helmet>
+        <title>屬靈恩賜評估結果</title>
+      </Helmet>
+      {showResults ? (
+        <div>
+          <h2 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
+            屬靈恩賜評估結果
+          </h2>
+          <div className="pb-8">
+            <CustomPieChart results={results} />
+          </div>
+          <Table>
+            <TableCaption>你的屬靈恩賜評估結果</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="font-bold">項目</TableHead>
+                <TableHead className="text-right">分數</TableHead>
+                <TableHead className="text-right">排名</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {results.map((result) => (
+                <ResultCategory key={result.name} result={result} />
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      ) : (
+        <div>
+          <Alert className="hover:bg-zinc-100 dark:hover:bg-zinc-800 mb-4">
+            <Clock className="h-4 w-4" />
+            <AlertTitle>
+              Result will be revealed during the Barnabas Camp
+            </AlertTitle>
+            <AlertDescription>
+              下載呢個程序到你手機， 入camp嘅時候再返嚟睇個結果 🙏
+              <br />
+              Download this assessment to your phone and check the results when
+              you come to camp 🙏
+            </AlertDescription>
+          </Alert>
+          <CountdownClock />
+        </div>
+      )}
     </div>
-    <Table>
-      <TableCaption>你的屬靈恩賜評估結果</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="font-bold">項目</TableHead>
-          <TableHead className="text-right">分數</TableHead>
-          <TableHead className="text-right">排名</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {results.map((result) => (
-          <ResultCategory key={result.name} result={result} />
-        ))}
-      </TableBody>
-    </Table>
-  </div>
-);
+  );
+};
 
 export default ValidResults;
